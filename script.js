d58826a4db4504a7233c82244f20551dbe6e672f
luciano-mesquita <52318257+luciano-mesquita@users.noreply.github.com>
2021-09-15 15:03:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,176 @@
-document.querySelector('.busca').addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    let input = document.querySelector('#searchInput').value;
-
-    if (input != '') {
-        clearInfo();
-        showWarning('Carregando...');
-
-        let url = `https://api.coingecko.com/api/v3/coins/${encodeURI(input)}`;
-
-        let results = await fetch(url);
-        let json = await results.json();
-
-        if (json) {
-            showInfo({
-                name: json.name,
-                symbol: json.symbol,
-                icon: json.image.small,
-                price: json.market_data.current_price.brl,
-                description: json.description.pt,
-                market_cap: json.market_data.market_cap.brl,
-                rank: json.coingecko_rank,
-                change_1d: json.market_data.price_change_percentage_24h,
-                change_7d: json.market_data.price_change_percentage_7d,
-                change_1y: json.market_data.price_change_percentage_1y,
-                max_supply: json.market_data.max_supply,
-                circulating_supply: json.market_data.circulating_supply,
-            })
-        } else {
-            clearInfo();
-            showWarning('Não localizamos esse projeto.');
-        }
-    }
-});
-
-function showInfo(json) {
-    showWarning('');
-
-    document.querySelector('.result').style.display = 'block';
-    document.querySelector('.name').innerHTML = `-- ${json.name.toUpperCase()} --`;
-    document.querySelector('.result img').setAttribute('src', `${json.icon}`);
-    document.querySelector('.symbol').innerHTML = `${json.symbol.toUpperCase()}`;
-    document.querySelector('.price').innerHTML = `${json.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`;
-    document.querySelector('.marketCap').innerHTML = `${json.market_cap.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`;
-    document.querySelector('.rank').innerHTML = `${json.rank}`;
-    document.querySelector('.change1d').innerHTML = `${addIndicator(json.change_1d)}`;
-    document.querySelector('.change7d').innerHTML = `${addIndicator(json.change_7d)}`;
-    document.querySelector('.change1y').innerHTML = `${addIndicator(json.change_1y)}`;
-    document.querySelector('.maxSupply').innerHTML = `${showSupply(json.max_supply)}`;
-    document.querySelector('.circulatingSupply').innerHTML = `${json.circulating_supply}`;
-    document.querySelector('.description').innerHTML = `${linkRemove(json.description)}`;
-
-    setColor(json.change_1d, '#val1');
-    setColor(json.change_7d, '#val2');
-    setColor(json.change_1y, '#val3');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-function linkRemove(data) {
-    let replace = data.replace(/<a\b[^>]*>/gm, '').replace(/<\/a>/gm, '');
-    console.log(replace);
-    return replace;
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: #333;
+    font-family: Arial, Helvetica, sans-serif;
 }
 
-function setColor(data, id) {
-    if (data > 0) {
-        document.querySelector(id).style.color = 'chartreuse';
-    } else {
-        document.querySelector(id).style.color = 'red';
-    }
+h1 {
+    color: #FFF;
+    margin: 20px 0;
 }
 
-function clearInfo() {
-    showWarning('');
-    document.querySelector('.result').style = 'none';
+.busca {
+    display: flex;
+    width: 750px;
 }
 
-function showWarning(msg) {
-    document.querySelector('.aviso').innerHTML = msg;
+.busca #searchInput {
+    flex: 1;
+    padding: 10px;
+    outline: none;
+    font-size: 15px;
+    border-radius: 5px;
+    border: 0;
 }
 
-function showSupply(data) {
-    if (data == null) {
-        data = 'Emissão Ilimitada';
-        return data;
-    } else {
-        return data;
-    }
+.busca button {
+    padding: 10px;
+    font-size: 15px;
+    border: 0;
+    background-color: #555;
+    color: #FFF;
+    border-radius: 5px;
+    margin-left: 10px;
+    cursor: pointer;
+    border: 1px solid #fff;
 }
 
-function addIndicator(data) {
-    if (data > 0) {
-        return `+${data.toFixed(2)}%`;
-    } else {
-        return `${data.toFixed(2)}%`;
-    }
+.busca button:hover {
+    background-color: #444;
+}
+
+.result {
+    width: 750px;
+    display: none;
+    border: 1px solid chartreuse;
+    margin-top: 20px;
+    border-radius: 5px;
+    background-color: #222;
+}
+
+.name {
+    width: 100%;
+    color: #FFF;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    padding: 20px;
+}
+
+.icon {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.name .icon {
+    float: left;
+}
+
+.icon {
+    padding: 20px;
+}
+
+.info {
+    display: flex;
+    width: 100%;
+}
+
+.info .col1 {
+    flex: 1;
+    text-align: center;
+}
+
+.info .col2 {
+    flex: 1;
+    text-align: center;
+}
+
+.info .col3 {
+    flex: 1;
+    text-align: center;
+}
+
+.symbol {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.description {
+    color: #777;
+    font-size: 16px;
+    margin-bottom: 5px;
+    text-align: justify;
+    padding: 30px;
+    padding-top: 0px;
+}
+
+.circulatingSupply,
+.maxSupply {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.titles {
+    color: #777;
+    font-size: 16px;
+    margin-bottom: 5px;
+}
+
+#val1 {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+#val2 {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+#val3 {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.rank,
+.price,
+.marketCap {
+    color: #FFF;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.sigla .sup {
+    font-size: 15px;
+    font-weight: normal;
+}
+
+.price span {
+    font-size: 15px;
+    font-weight: normal;
+}
+
+.aviso {
+    color: #FFF;
+    padding-top: 20px;
+}
+
+footer {
+    margin-top: 200px;
+    color: #FFF;
+    font-size: 13px;
 }
