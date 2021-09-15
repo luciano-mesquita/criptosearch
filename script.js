22e069c95161288bccb56f9f902ab6061154bf5e
Luciano Mesquita <lucianomesquita01@gmail.com>
2021-09-15 09:23:19 -0300

    create functions

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.querySelector('.busca').addEventListener('submit', async(event)=> {
                 rank: json.coingecko_rank,
                 change_1d: json.market_data.price_change_percentage_24h,
                 change_7d: json.market_data.price_change_percentage_7d,
+                max_supply: json.market_data.max_supply,
+                circulating_supply: json.market_data.circulating_supply,
             })
         } else {
             clearInfo();
@@ -35,14 +37,16 @@ function showInfo(json) {
     showWarning('');
 
     document.querySelector('.result').style.display = 'block';
-    document.querySelector('.name').innerHTML = `Projeto: ${json.name}`;
+    document.querySelector('.name').innerHTML = `-- ${json.name} --`;
     document.querySelector('.result img').setAttribute('src', `${json.icon}`);
     document.querySelector('.symbol').innerHTML = `${json.symbol.toUpperCase()}`;
     document.querySelector('.price').innerHTML = `${json.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`;
     document.querySelector('.marketCap').innerHTML = `${json.market_cap.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`;
     document.querySelector('.rank').innerHTML = `${json.rank}`;
-    document.querySelector('.change1d').innerHTML = `${json.change_1d.toFixed(2)}%`;
-    document.querySelector('.change7d').innerHTML = `${json.change_7d.toFixed(2)}%`;
+    document.querySelector('.change1d').innerHTML = `${formatVariation(json.change_1d)}`;
+    document.querySelector('.change7d').innerHTML = `${formatVariation(json.change_7d)}`;
+    document.querySelector('.maxSupply').innerHTML = `${showSupply(json.max_supply)}`;
+    document.querySelector('.circulatingSupply').innerHTML = `${json.circulating_supply}`;
     document.querySelector('.description').innerHTML = `${json.description}`;
 }
 
@@ -53,4 +57,22 @@ function clearInfo() {
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
+}
+
+function showSupply(data) {
+    if(data == null) {
+        data = 'Emissão Ilimitada';
+        return data;
+    } else {
+        return data;
+    }
+}
+
+function formatVariation(data) {
+    if(data > 0) {
+        let mount = `+${data.toFixed(2)}%`;
+        return mount;
+    } else {
+        return `${data.toFixed(2)}%`;
+    }
 }
